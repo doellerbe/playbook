ab162cad3b5f7e44748606b58b3b60d6ce302d3a
Quincy Iheme <qiheme@gmail.com>
2015-06-05 19:46:46 -0400

    add first user test and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: {
+    minimum: 2,
+    maximum: 30,
+    too_short: "You must have at least %{count} characters",
+    too_long: "You must have at most %{count} characters"
+  }
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, length: { in: 6..20 }
+  validates :administrator, presence: true
+  validates :moderator, presence: true
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when it is created" do
+    it "must contain at least two characters" do
+      user = User.new(name: "Q", email: "qiheme@gmail.com", password_digest: "Lucky!")
+      expect(user.save).to be false
+    end
+  end
 end
